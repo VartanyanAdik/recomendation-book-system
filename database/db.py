079dba7e92575f41334fb36e9e72b3934501ccbf
VartanyanAdik <vartanyan.arkadi@gmail.com>
2022-10-01 20:32:36 +0300

    Авторизация. Сравнения введенного пароля с паролям из баззы данных.

--- database/db.py
+++ database/db.py
@@ -1,6 +1,7 @@
+from crypto.cipher import password_encryption
 from entity.user import User
 
-database_set = set()
+database_dict = dict()
 test_user_one = User("Arkadi", "Vartanyan", "Gevorkovich", "adik508", "ad123")
 test_user_two = User("Ivan", "Ivanov", "Ivanovich", "ivan508", "ivan123")
 test_user_three = User("Artem", "Artemov", "Artemovich", "artem508", "artem123")
@@ -8,11 +9,15 @@ test_user_four = User("Vasili", "Vasilev", "Vasilevich", "vasili508", "vasili123
 
 
 def save_database(user):
-    database_set.add(user)
+    database_dict[user.login] = user
     print("Пользователь успешно сохранен!")
+    print(f"Пользователь {user.login} успешно добавлен!")
 
 
 save_database(test_user_one)
 save_database(test_user_two)
 save_database(test_user_three)
 save_database(test_user_four)
+
+def check_user(login, password):
+    return login in database_dict and database_dict[login].password == password_encryption(password)
